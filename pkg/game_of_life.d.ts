3d6461f78a79ecb44ed421b1849bdd0dbdc3b80e
wxx <wangxingyuan@nnuo.com>
2023-03-02 17:32:03 +0800

    buid: 添加wasm文件夹

---
+++ pkg/game_of_life.d.ts
@@ -0,0 +1,58 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead = 0,
+  Alive = 1,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* @returns {number}
+*/
+  cells(): number;
+/**
+* @param {number} width
+*/
+  set_width(width: number): void;
+/**
+* @param {number} height
+*/
+  set_height(height: number): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {string}
+*/
+  render(): string;
+/**
+* @param {number} row
+* @param {number} column
+*/
+  toggle_cell(row: number, column: number): void;
+/**
+* @param {number} row
+* @param {number} column
+* @param {number} cell_status
+*/
+  change_cell(row: number, column: number, cell_status: number): void;
+/**
+*/
+  destory(): void;
+}
